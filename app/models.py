2d9d9202b18b66c9c9ab31e00955d3e1605d90db
afidegnum <afidegnum@yahoo.com>
2017-06-14 05:07:17 +0000

    Error during master/detail process

---
+++ app/models.py
@@ -0,0 +1,135 @@
+from flask import url_for
+from flask_appbuilder import Model
+from flask_appbuilder.filemanager import ImageManager
+from flask_appbuilder.models.mixins import AuditMixin, FileColumn, ImageColumn
+from markupsafe import Markup
+from sqlalchemy import Column, Integer, String, ForeignKey, Text, Date
+from sqlalchemy.orm import relationship
+import datetime
+
+
+def today():
+    return datetime.datetime.today().strftime('%Y-%m-%d')
+
+
+class Media(Model):
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), unique=True, nullable=False)
+    path = Column(String(250), unique=True, nullable=False)
+    photo = Column(ImageColumn(thumbnail_size=(30, 30, True), size=(300, 300, True)))
+    posted = Column(Date, default=today, nullable=False)
+    publications = relationship("Publication")
+    publications_id = Column(Integer, ForeignKey('publication.id'), nullable=False)
+    news = relationship("News")
+    news_id = Column(Integer, ForeignKey('news.id'), nullable=False)
+    activities = relationship("Activity")
+    activities_id = Column(Integer, ForeignKey('activity.id'), nullable=False)
+    messages = relationship("Message")
+    messages_id = Column(Integer, ForeignKey('message.id'), nullable=False)
+
+    def photo_img(self):
+        im = ImageManager()
+        if self.photo:
+            return Markup('<a href="' + url_for('MediaModelView.show', pk=str(self.id)) + \
+                          '" class="thumbnail"><img src="' + im.get_url(self.photo) + \
+                          '" alt="Photo" class="img-rounded img-responsive"></a>')
+        else:
+            return Markup('<a href="' + url_for('MediaModelView.show', pk=str(self.id)) + \
+                          '" class="thumbnail"><img src="//:0" alt="Photo" class="img-responsive"></a>')
+
+    def photo_img_thumbnail(self):
+        im = ImageManager()
+        if self.photo:
+            return Markup('<a href="' + url_for('MediaModelView.show', pk=str(self.id)) + \
+                          '" class="thumbnail"><img src="' + im.get_url_thumbnail(self.photo) + \
+                          '" alt="Photo" class="img-rounded img-responsive"></a>')
+        else:
+            return Markup('<a href="' + url_for('MediaModelView.show', pk=str(self.id)) + \
+                          '" class="thumbnail"><img src="//:0" alt="Photo" class="img-responsive"></a>')
+
+
+
+
+
+    def __repr__(self):
+        return self.name
+
+
+
+class About(Model):
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), unique=True, nullable=False)
+    body = Column(Text(50), unique=True, nullable=False)
+
+    def __repr__(self):
+        return self.name
+
+
+class News(Model):
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), unique=True, nullable=False)
+    body = Column(Text(50), unique=True, nullable=False)
+    posted_date = Column(Date, default=today, nullable=False)
+    media = relationship('Media')
+
+    def photo_img(self):
+        im = ImageManager()
+        if self.photo:
+            return Markup('<a href="' + url_for('NewsModelView.show', pk=str(self.id)) + \
+                          '" class="thumbnail"><img src="' + im.get_url(self.photo) + \
+                          '" alt="Photo" class="img-rounded img-responsive"></a>')
+        else:
+            return Markup('<a href="' + url_for('NewsModelView.show', pk=str(self.id)) + \
+                          '" class="thumbnail"><img src="//:0" alt="Photo" class="img-responsive"></a>')
+
+    def photo_img_thumbnail(self):
+        im = ImageManager()
+        if self.photo:
+            return Markup('<a href="' + url_for('NewsModelView.show', pk=str(self.id)) + \
+                          '" class="thumbnail"><img src="' + im.get_url_thumbnail(self.photo) + \
+                          '" alt="Photo" class="img-rounded img-responsive"></a>')
+        else:
+            return Markup('<a href="' + url_for('NewsModelView.show', pk=str(self.id)) + \
+                          '" class="thumbnail"><img src="//:0" alt="Photo" class="img-responsive"></a>')
+
+    def __repr__(self):
+        return self.name
+
+class Publication(Model):
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), unique=True, nullable=False)
+    body = Column(Text(50), unique=True, nullable=False)
+    published_date = Column(Date, default=today, nullable=False)
+
+    def __repr__(self):
+        return self.name
+
+class Activity(Model):
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), unique=True, nullable=False)
+    body = Column(Text(50), unique=True, nullable=False)
+    start_date = Column(Date, default=today, nullable=False)
+    end_date = Column(Date, nullable=True)
+
+    def __repr__(self):
+        return self.name
+
+class Message(Model):
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), unique=True, nullable=False)
+    body = Column(Text(50), unique=True, nullable=False)
+    posted = Column(Date, default=today, nullable=False)
+
+
+    def __repr__(self):
+        return self.name
+
+"""
+
+You can use the extra Flask-AppBuilder fields and Mixin's
+
+AuditMixin will add automatic timestamp of created and modified by who
+
+
+"""
+        
---
+++ app/views.py
@@ -0,0 +1,100 @@
+from flask import render_template
+from flask_appbuilder.models.sqla.interface import SQLAInterface
+from flask_appbuilder.views import MasterDetailView
+from flask_appbuilder import ModelView, BaseView
+from .models import About, News, Publication, Activity, Message, Media
+from app import appbuilder, db
+from flask_appbuilder.widgets import ListThumbnail, ListAddWidget, FormInlineWidget, ListMasterWidget
+
+
+class MediaView(ModelView):
+
+    datamodel = SQLAInterface(Media)
+
+    list_columns = ['name', 'news', 'activities', 'publications', 'messages', 'posted']
+
+
+    add_fieldsets = [
+        ('Media', {'fields': ['photo']})
+    ]
+
+    edit_fieldsets = [
+        ('Media', {'fields': ['photo']})
+    ]
+
+
+class AboutView(ModelView):
+    datamodel = SQLAInterface(About)
+
+
+
+class NewsView(MasterDetailView):
+    datamodel = SQLAInterface(News)
+
+    list_widget = ListMasterWidget
+
+    list_columns = ['name','posted_date', 'media']
+    label_columns = {'name':'News Title', 'posted_date':'Posted On', 'media':'Gallery'}
+    related_views = [MediaView]
+
+
+class PublicationView(ModelView):
+
+    datamodel = SQLAInterface(Publication)
+    list_columns = ['name','published_date']
+    related_views = [MediaView]
+    list_widget = ListThumbnail
+
+
+class ActivityView(ModelView):
+    datamodel = SQLAInterface(Activity)
+    list_columns = ['name', 'start_date', 'end_date']
+    related_views = [MediaView]
+    list_widget = ListThumbnail
+
+
+
+class MessageView(ModelView):
+
+    datamodel = SQLAInterface(Message)
+    list_columns = ['name', 'posted']
+    related_views = [MediaView]
+    list_widget = ListThumbnail
+
+
+
+db.create_all()
+
+appbuilder.add_view(AboutView, "About", icon="fa-folder-open-o", category="Admin")
+appbuilder.add_view(NewsView, "News", icon="fa-folder-open-o", category="Admin")
+appbuilder.add_view(PublicationView, "Publications", icon="fa-folder-open-o", category="Admin")
+appbuilder.add_view(ActivityView, "Activities", icon="fa-folder-open-o", category="Admin")
+appbuilder.add_view(MessageView, "Messages", icon="fa-folder-open-o", category="Admin")
+appbuilder.add_view(MediaView, "Media", icon="fa-folder-open-o", category="Admin")
+
+
+
+"""
+    Create your Views::
+
+
+    class MyModelView(ModelView):
+        datamodel = SQLAInterface(MyModel)
+
+
+    Next, register your Views::
+
+
+    appbuilder.add_view(MyModelView, "My View", icon="fa-folder-open-o", category="My Category", category_icon='fa-envelope')
+"""
+
+"""
+    Application wide 404 error handler
+"""
+@appbuilder.app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html', base_template=appbuilder.base_template, appbuilder=appbuilder), 404
+
+
+
+
